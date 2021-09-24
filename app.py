7071a50d0910f644f88015a59a6670eb4ce274bf
perfumescent <31856209+perfumescent@users.noreply.github.com>
2021-09-25 01:28:14 +0800

    route1

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import escape
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Welcome to My Watchlist!'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s\n%s' % (escape(name),name)
