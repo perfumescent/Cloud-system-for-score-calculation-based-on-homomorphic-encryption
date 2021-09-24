b9c828bf8ed1bcd543dac3e0f782626c4c7e3162
perfumescent <31856209+perfumescent@users.noreply.github.com>
2021-09-25 07:51:09 +0800

    database csv

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-from flask import escape
+from flask import Flask, render_template, escape
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
