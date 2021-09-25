187ecc2b980d392cf1a7f74b887e85d84aa1f0c6
perfumescent <31856209+perfumescent@users.noreply.github.com>
2021-09-25 21:40:39 +0800

    index 页面完成

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, url_for, redirect
+from flask import Flask, render_template, request, flash, url_for, redirect, session
 import pandas as pd
 import rsa
 
@@ -7,18 +7,42 @@ app.config['SECRET_KEY'] = 'dev'
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        id = int(request.form.get('id'))
+        data = pd.read_csv("user.csv")
+
+        if len(data.loc[data.id==id])==0:
+            flash("用户不存在，请先申请安全令！")
+            return render_template('register.html')
+
+        session['id']=id
+
+        if data.loc[data.id==id].identity is None:
+            return redirect(url_for('set'))
+        
+        return redirect(url_for('authentication'))
+        
     return render_template('index.html')
+
+
+@app.route('/set', methods=['GET', 'POST'])
+def set():
+    return render_template('set.html')
+
+@app.route('/authentication', methods=['GET', 'POST'])
+def authentication():
+    return render_template('authentication.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        id = request.form.get('id')
+        id = int(request.form.get('id'))
         name = request.form.get('name')
         status = request.form.get('status')
         data = pd.read_csv("user.csv")
         
-        if len(data[data.id==int(id)])>0:
-            flash("你已经申请过安全令！")
+        if len(data.loc[data.id==id])>0:
+            flash("你已经申请过安全令，请勿重复！")
             return render_template('register.html')
         
         (pub, pri) = rsa.newkeys(512)
